cb5cae01ee38d7aa461ed7b2c0a37b4022286281
arcan1s <darkarcanis@mail.ru>
2015-03-27 20:13:18 +0300

    create working tree

---
+++ sources/quadro-core/docs.cmake
@@ -0,0 +1,12 @@
+# doxygen documentation
+find_package (Doxygen)
+
+configure_file (doxygen.conf.in ${CMAKE_CURRENT_BINARY_DIR}/doxygen.conf)
+add_custom_target (oxygen-docs ALL COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/doxygen.conf)
+
+# man pages
+# workaround for man pages
+set (MAN_DIR ${CMAKE_CURRENT_BINARY_DIR}/docs/man/man3)
+# install (FILES ${MAN_DIR}/Netctl.3 DESTINATION share/man/man3 RENAME ${SUBPROJECT}_Netctl.3)
+# html docs
+install (DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/docs/html DESTINATION share/doc/${PROJECT_NAME})
